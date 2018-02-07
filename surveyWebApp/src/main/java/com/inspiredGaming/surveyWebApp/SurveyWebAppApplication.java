8f88fb2f15e11164acbeaa1ba6f870b9fe57229d
leviroque <levroque@gmail.com>
2018-02-07 23:22:38 +0000

    test

--- surveyWebApp/src/main/java/com/inspiredGaming/surveyWebApp/SurveyWebAppApplication.java
+++ surveyWebApp/src/main/java/com/inspiredGaming/surveyWebApp/SurveyWebAppApplication.java
@@ -8,7 +8,7 @@ public class SurveyWebAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SurveyWebAppApplication.class, args);
-                System.out.println("hello");
-                System.out.println("Please if write your name");
+                System.out.println("hello world");
+                System.out.println("Please write your name");
 	}
 }
