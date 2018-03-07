4c47eff6c848a302a8550adbf6c1e2ce11de2d9c
Stefanos-Z <eeu823@bangor.ac.uk>
2018-03-07 12:25:11 +0000

    answerDao

---
+++ surveyWebApp/src/main/java/com/inspiredGaming/surveyWebApp/models/dao/AnswersDao.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.inspiredGaming.surveyWebApp.models.dao;
+
+import com.inspiredGaming.surveyWebApp.models.Answers;
+import java.util.List;
+import javax.transaction.Transactional;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Levi
+ */
+@Repository
+@Transactional
+public interface AnswersDao extends CrudRepository<Answers, Integer>{
+    
+    public List<Answers> findAll();
+    
+    public Answers findByAnswerId(Integer answerId);
+}
