f81ebcd031fa167ffd0086831caa8a16f14c814d
leviroque <levroque@gmail.com>
2018-03-07 12:23:30 +0000

    Added questions DAO file.

---
+++ surveyWebApp/src/main/java/com/inspiredGaming/surveyWebApp/models/dao/QuestionsDao.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.inspiredGaming.surveyWebApp.models.dao;
+
+import com.inspiredGaming.surveyWebApp.models.HelloLog;
+import com.inspiredGaming.surveyWebApp.models.Questions;
+import java.util.List;
+import javax.transaction.Transactional;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Levi
+ */
+@Repository
+@Transactional
+public interface QuestionsDao extends CrudRepository<Questions, Integer>{
+    
+    public List<Questions> findAll();
+    
+    public HelloLog findByQuestionId(Integer questionId);
+}
