41b3d3afef557a850d77d89d19568f1c1c21e44b
leviroque <levroque@gmail.com>
2018-03-10 21:10:42 +0000

    Added class to build and send emails.

--- surveyWebApp/src/main/java/com/inspiredGaming/surveyWebApp/Email.java
+++ surveyWebApp/src/main/java/com/inspiredGaming/surveyWebApp/Email.java
@@ -5,65 +5,73 @@
  */
 package com.inspiredGaming.surveyWebApp;
 
-import com.sun.xml.internal.messaging.saaj.packaging.mime.MessagingException;
 import java.util.Properties;
 import javax.mail.*;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 /**
  *
  * @author Levi
  */
 public class Email {
+        
+    private String emailAddress;
+    private String url;
+    
+    public Email(String emailAddress, String url)
+    {
+        this.emailAddress = emailAddress;
+        this.url = url;
+    }
+    
     /**
-	 * Sends an email to an address with an image attachment.
-	 * @throws AddressException
-	 * @throws MessagingException
-	 */
-	public static void emailSender() throws AddressException, MessagingException
-	{
-		//Set up SMTP details
-		Properties emailServer = new Properties();
-		emailServer.put("mail.smtp.host","smtp.gmail.com");
-		emailServer.put("mail.smtp.socketFactory.port", "465");
-		emailServer.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-		emailServer.put("mail.smtp.auth", "true");
-		emailServer.put("mail.smtp.port", "465");
-		
-		//Supply gmail authentication details. 
-		Session session = Session.getDefaultInstance(emailServer,
-				new javax.mail.Authenticator() {
-				protected PasswordAuthentication getPasswordAuthentication(){
-				return new PasswordAuthentication("emailsenderbangor@gmail.com","Abc.123456");
-				}
-			}
-		);
-		
-		//Compose and Send Message
-		Message message=new MimeMessage(session);
-		message.setFrom(new InternetAddress("levroque@gmail.com"));
-		message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("levroque@gmail.com"));
-		message.setSubject("Visitor Alert");
-        
-		// Set the text of the email message text.
+    * Sends an email to an address with link to a survey.
+    * @throws AddressException
+    * @throws MessagingException
+    */
+    public void send() throws AddressException, MessagingException
+    {
+
+        //Set up SMTP details
+        Properties emailServer = new Properties();
+        emailServer.put("mail.smtp.host","smtp.gmail.com");
+        emailServer.put("mail.smtp.socketFactory.port", "465");
+        emailServer.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        emailServer.put("mail.smtp.auth", "true");
+        emailServer.put("mail.smtp.port", "465");
+
+        //Supply gmail authentication details.
+        Session session = Session.getDefaultInstance(emailServer,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication(){
+                        return new PasswordAuthentication("emailsenderbangor@gmail.com","Abc.123456");
+                    }
+                }
+        );
+
+        //Compose and Send Message
+        Message message=new MimeMessage(session);
+        //message.setFrom(new InternetAddress("levroque@gmail.com"));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(emailAddress));
+        message.setSubject("Visitor Alert");
+
+        // Set the text of the email message text.
         MimeBodyPart messagePart = new MimeBodyPart();
-        messagePart.setText("Your house was visited on "+getDate()+". Please see the attached image");
-        
-        // Set the email attachment file
-        FileDataSource fileDataSource = new FileDataSource("C:\\Users\\Levi\\workspace\\ArduinoProject\\Visitor.jpg");
-        MimeBodyPart attachmentPart = new MimeBodyPart();
-        attachmentPart.setDataHandler(new DataHandler(fileDataSource));
-        attachmentPart.setFileName(fileDataSource.getName());
-		
+        messagePart.setText("You have been invited to complete the following survey:\n \n"+url);
+
         //Create the Email
         Multipart multipart = new MimeMultipart();
         multipart.addBodyPart(messagePart);
-        multipart.addBodyPart(attachmentPart);
-
         message.setContent(multipart);
 
-		Transport.send(message);
-		System.out.println("Email sent");
-		
-		
-	}
+        Transport.send(message);
+
+        //debug
+        System.out.println("Email sent");
+
+    }
 }
