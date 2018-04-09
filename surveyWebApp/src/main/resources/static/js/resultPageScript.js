2c08500e81fee87ec1b1a07e3446658b398d4f36
Loizos V. <eeu905@bangor.ac.uk>
2018-04-09 13:39:11 +0100

    update: display real values from db to p5js

--- surveyWebApp/src/main/resources/static/js/resultPageScript.js
+++ surveyWebApp/src/main/resources/static/js/resultPageScript.js
@@ -1,21 +1,27 @@
 /* Variables Declaration */
-var canvasWidth = 2000;
-var canvasHeight = 600;
 var pieChartSize = 400;
-var respondentDataInputArray = [100,200,500,300,450]; //Holds the number of answers for each question
-var sum = respondentDataInputArray[0]+respondentDataInputArray[1]+respondentDataInputArray[2]
-        +respondentDataInputArray[3]+respondentDataInputArray[4]; //Holds the sum of the answers
+var respondentDataInputArray = [1]; //Holds the number of answers for each question
+var sum = total(respondentDataInputArray); //Holds the sum of the answers
+
 var percentagesArray = [respondentDataInputArray.length]; //Depending on each answer from 5 seperete answers by persentage
 var chartArray = [respondentDataInputArray.length]; //Holds 5 arcs to present a pie chart
 var answersArray = ["Strongly Agree","Agree","Neutral","Disagree","Strongly Disagree"];
 var rectX = 500; //holds space between the status rectangles (in width)
 var rectY = 100; //holds space between the status rectangles (in height)
 
+var canvasWidth = 1000;
+var canvasHeight = respondentDataInputArray.length * 100;
+
 /* SETUP CANVAS */
 function setup() {
   
-  var canvas = createCanvas(2000, 600); //Create the Canvas to draw the Pie Chart
-  canvas.parent("question1");
+  if(canvasHeight > 550){
+    var canvas= createCanvas(canvasWidth, canvasHeight); //Create the Canvas to draw the Pie Chart
+    canvas.parent("question1");
+  }else{
+    var canvas = createCanvas(canvasWidth, 550);
+    canvas.parent("question1");
+  }
   background(255); //Set a White Background Color on the Canvas
   
   /* Draw Chart after calculation */
@@ -51,14 +57,19 @@ function pieChart(diameter, data) {
   
   for (var i=0; i<data.length; i++) {
         
-    fill(random(255),random(255),random(255)); //Add random Color
+    if(chartArray[i]>0){
+        
+
+        fill(random(255),random(255),random(255)); //Add random Color
+
+        /* DRAW PIE CHART */
+        arc(25+pieChartSize/2, height/2-50, 	//Arguments : x/y coordinate of the arc's ellipse
+            diameter, diameter, 											  	//width/height of the arc's ellipse by default
+            lastAngle, lastAngle+radians(chartArray[i]));	//angle to start/stop the arc, specified in radians
+
+        lastAngle += radians(chartArray[i]); //Add to last angle depenting on the next Arc
+    }
     
-    /* DRAW PIE CHART */
-    arc(25+pieChartSize/2, height/2-50, 	//Arguments : x/y coordinate of the arc's ellipse
-        diameter, diameter, 											  	//width/height of the arc's ellipse by default
-        lastAngle, lastAngle+radians(chartArray[i]));	//angle to start/stop the arc, specified in radians
-    
-    lastAngle += radians(chartArray[i]); //Add to last angle depenting on the next Arc
     
     /* DRAW STATUS OF THE PIE CHART */
     rect(rectX,rectY,20,20); 
@@ -66,10 +77,19 @@ function pieChart(diameter, data) {
     textSize(20); //Change the size of the text
     
     var percentagesText = text(nf(percentagesArray[i], 2,2), rectX+35, rectY+15); //Remove Decimal numbers
-    var str = "         % , " + answersArray[i] + " , ("+respondentDataInputArray[i]+" people answered)";
+    var str = "          % , " + answersArray[i] + " , ("+respondentDataInputArray[i]+" people answered)";
     text(str,rectX+35,rectY+15);
     
     rectY+=80; //Move next point of status
     
   } //-End of For Loop 
+}
+
+function total(array){
+    var sum = 0;
+    
+    for(var i=0; i<array.length; i++){   
+        sum+= array[i];
+    }
+    return sum;
 }
