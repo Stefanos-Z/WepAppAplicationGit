65b73fdc95e0f9a5313ffa668f8053a5e1ea4a83
Loizos V. <eeu905@bangor.ac.uk>
2018-04-16 00:23:55 +0100

    Stefanos and I had changed several things in this script. Now it accepts an array of numbers that would create the stacked bars. More information coming soon

--- surveyWebApp/src/main/resources/static/js/stackedBarChart.js
+++ surveyWebApp/src/main/resources/static/js/stackedBarChart.js
@@ -1,32 +1,34 @@
 /* Needed Variables Declaration */
-var month1 = ["M01V01", "M01V02","M01V03","M01V04"];
-var month2 = ["M02V01", "M02V02", "M02V03", "M02V04", "M02V05"];
-var month3 = ["M03V01"];
-var month4 = ["M04V01", "M04V02", "M04V03"];
+var month1 = [6,7,20,6,5];
+var month2 = [9,8,7,6,5];
+var month3 = [7,7,7,7,7];
+var month4 = [1,2,3,42,5];
 
 var arrayOfMonths = [month1, month2, month3, month4]; //X Axis (Could be String or Array)
-var arrayOfResponses = ["1","2","3","4","5","6"];     //Y Axis (Could be String ONLY)
+//var arrayOfResponses = ["1","2","3","4","5"];     //Y Axis (Could be String ONLY)
 
 /* Calculated Variables */
 var numOfMonths = arrayOfMonths.length;
-var numOfResponses = arrayOfResponses.length;
+var numOfResponses =  arrayOfMonths[0].length;
 
 /* Other Variable Declarations */
-var extraSpace = 250;
-var canvasWidth = (numOfMonths * 100) + extraSpace;
-var canvasHeight = (numOfResponses * 100) + extraSpace;
+var extraSpace = 400;
+// var canvasWidth = (numOfMonths * 100) + extraSpace;
+// var canvasHeight = (numOfResponses * 100) + extraSpace;
+var canvasWidth =800;
+var canvasHeight = 600;
 
 function setup() {
 
     //Create the canvas to display the Stacked Bar Chart 
-    createCanvas(canvasWidth+100, canvasHeight); 
+    createCanvas(canvasWidth, canvasHeight); 
     background(255); /* !!!  (REMOVE THIS TO MAKE BACKGROUND TRANSPARENT)  !!! */
 
     chartBase(); //Add X,Y Axis Lines
+    var maxNumOfResondents = findMaxNumOfResondents();
+    //addPointers(numOfMonths, maxNumOfResondents); //Add Pointers to X,Y Axis
 
-    addPointers(numOfResponses, numOfMonths); //Add Pointers to X,Y Axis
-
-    addTextToPointers(); //Add Text to pointers of Y Axis
+    // addTextToPointers(); //Add Text to pointers of Y Axis
 
     addValues(); //Add the Bars (Values) of the Chart
 }
@@ -37,8 +39,7 @@ function setup() {
 function chartBase() {
 
   /* DRAW X AXIS LINE */
-  line(canvasWidth - (canvasWidth - 50), canvasHeight - 100,
-    (canvasWidth - 50) + 25, canvasHeight - 100);
+  line(50,canvasHeight-50,canvasWidth-50,canvasHeight-50,);
 
   /* DRAW Y AXIS LINE */
   line(canvasHeight - (canvasHeight - 100), canvasHeight - (canvasHeight - 50) - 25,
@@ -53,25 +54,21 @@ function chartBase() {
 function addPointers(numOfMonths, numOfResponses) {
 
   /* Find space Between Pointers */
-  var spaceBetweenX = canvasWidth - (canvasWidth - 150);
-  var spaceBetweenY = canvasHeight - 150;
-
-  /* Count how many pointers */
-  numOfMonths = arrayOfMonths.length + 1; //+1 - ????
-  numOfResponses = arrayOfResponses.length+1; //+1 - ????
+  var spaceBetweenX = canvasWidth - (canvasWidth - 200);
+  var spaceBetweenY = canvasHeight -215;
   
   fill(0, 0, 0); //Black Color for the Dots (Pointers)
   
   /* ADD POINTERS TO X AXIS */
-  for (var x = 0; x < numOfMonths; x++) {
-    ellipse(spaceBetweenX, canvasHeight - 100, 5, 5);
-    spaceBetweenX += canvasWidth / numOfMonths;
+  for (var x = 0; x<numOfMonths; x++) {
+    ellipse(spaceBetweenX, canvasHeight - 50, 5, 5);
+    spaceBetweenX += (canvasWidth / numOfMonths)-25; //Change width here
   }
 
   /* ADD POINTERS TO Y AXIS */
-  for (var y = 0; y < arrayOfResponses.length; y++) {
+  for (var y = 0; y < numOfResponses; y++) {
     ellipse(canvasWidth - (canvasWidth - 100), spaceBetweenY, 5, 5);
-    spaceBetweenY -= canvasHeight / numOfResponses;
+    spaceBetweenY -= (canvasHeight / numOfResponses) - 85; //Change height here
   }
 }
 
@@ -79,8 +76,9 @@ function addTextToPointers() {
 
   var spaceBetweenX = canvasWidth - (canvasWidth - 130);
   var spaceBetweenY = (canvasHeight - 100) - 50;
-  textSize(13); //Set text size
+  textSize(15); //Set text size
 
+  
   /* ADD TEXT TO Y AXIS */
   for (var y = 0; y < arrayOfResponses.length; y++) {
 
@@ -92,28 +90,62 @@ function addTextToPointers() {
 
 function addValues() {
 
-  var locX = canvasWidth - (canvasWidth - 125);
-  var locY = canvasHeight - 215;
-  var barWidth = 50;
-  var barData = canvasHeight / (numOfResponses + 2);
-
+  var locX = canvasWidth - (canvasWidth - 170); //Start Location X
+  //var startLocationY = canvasHeight - 215; //Start Location Y
+  
+  
+  var barWidth = 60;
+  var barHeight;
+  var locY;
   for (var x = 0; x < arrayOfMonths.length; x++) {
+    
+     
     /* Draw Stack Bars (rectangles) */
     for (var y = 0; y < arrayOfMonths[x].length; y++) {
-
+      barHeight =  arrayOfMonths[x][y] * 8;
+      if(y==0){
+        locY = canvasHeight-barHeight-50;
+        
+      }else{
+        locY -= barHeight;
+      }
+      
       /* ADD RECTANGLES TO Y AXIS */
       fill(random(255), random(255), random(255));
-      rect(locX, locY, barWidth, barData);
-
+      rect(locX, locY, barWidth, barHeight);
+      
       /* ADD TEXT TO X AXIS */
-      fill(0, 0, 0); //Black Color For Text
-      text(arrayOfMonths[x][y], locX, locY + 50);
+      fill(255,255,255); //White Color For Text
+      text(arrayOfMonths[x][y], locX+25, locY+15);
 
-			locY -= 115;
+			
     }
     
 
-    locX += 130;
-    locY = canvasHeight - 215;
+    locX += 135;
+    //locY = canvasHeight - 215; //Start Location on Y Axis (DONT CHANGE)
   }
 }
+
+
+function findMaxNumOfResondents(){
+
+  var myMax = 0;
+  for(var i = 0; i<numOfMonths; i++){
+    var thisMax = 0;
+    for(var j= 0; j<numOfResponses; j++){
+        thisMax+= arrayOfMonths[i][j];
+      }
+      if(thisMax>myMax)
+      {
+        myMax = thisMax;
+      }
+    }
+    return myMax
+}
+
+
+
+
+
+
