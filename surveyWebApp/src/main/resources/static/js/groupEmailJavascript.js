bdfc890a6abd7347bce3b5152d025f6a0a09a02c
oneZt <oneZt@192.168.1.2>
2018-05-04 16:46:50 +0100

    comments

--- surveyWebApp/src/main/resources/static/js/groupEmailJavascript.js
+++ surveyWebApp/src/main/resources/static/js/groupEmailJavascript.js
@@ -4,16 +4,18 @@
  * and open the template in the editor.
  */
 
-$(document).ready(function() {
+$(document).ready(function() {//only rune when page is fully loaded
     $('#groupSelect').change(function(){
-        console.log($('#groupSelect option:selected').text());
+        //console.log($('#groupSelect option:selected').text());
         
         var group = {
-            groupName : $('#groupSelect option:selected').text()
+            groupName : $('#groupSelect option:selected').text()//gets selected group from the drop down menu
         }
         
+        //If the group selected is no New Group
         if(group.groupName !== "New Group")
         {
+            //ajax post
             $.ajax({
                 type : "POST",
                 contentType : "application/x-www-form-urlencoded",
@@ -31,9 +33,9 @@ $(document).ready(function() {
             });
             $('#newGroupName').val(group.groupName);//makes the value in the text field equal to the selected group name
             $('#groupEmailDelete').prop('disabled', false);// enebles delete button
-            $("#saveEmails").val("Update Email Group");
+            $("#saveEmails").val("Update Email Group");//update the text of th button
         }else{
-            $("#saveEmails").val("Save new Group");
+            $("#saveEmails").val("Save new Group");//update the text of th button
             $('#newGroupName').val("");//empties the new group name
             $("#emailList").val('');//empties the emial list
             $('#groupEmailDelete').prop('disabled', true);//disables delete button
